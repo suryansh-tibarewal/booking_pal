86cdc4c16052d8e4df03d382da1cbdc1cfa2039d
Suryansh Tibarewala <suryansh.tibarewala@booking.com>
2018-01-17 13:34:12 +0100

    merge

--- booking_pal.py
+++ booking_pal.py
@@ -7,6 +7,8 @@ import re
 import random
 from transitions import Machine
 from transitions import State
+import json
+from difflib import SequenceMatcher
 
 # --------------------------------------------------------------------------------------------
 # INITIALISATION
@@ -61,6 +63,68 @@ def followUpWelcome():
 def followUpWelcome():
     rijksmuseum_details = render_template('rijksmuseum_details')
     return question(rijksmuseum_details)
+
+
+last_node = ""
+def parse_json_file():
+    json_file = None
+    with open('info.json') as json_data:
+        json_file = json.load(json_data)
+    return json_file
+
+json_file = parse_json_file()
+
+def get_closest_mueseum(museum):
+    word, cnt = None, 0
+    for m in json_file['museums'].keys():
+        print m
+        temp = SequenceMatcher(None, museum, m).ratio()
+        if temp >= cnt:
+            cnt = temp
+            word = m
+    return word
+
+@ask.intent("ListMuseums")
+def list_museums():
+    global last_node
+    last_node = "ListMuseums"
+    museums = ", ".join(json_file['museums'].keys())
+    output = render_template('list_museums', museums=museums)
+    return question(output)
+
+@ask.intent("MuseumsOpeningTime")
+def museums_opening_time(museum):
+    global last_node
+    last_node = "MuseumsOpeningTime"
+    museum_guess = get_closest_mueseum(museum)
+    session.attributes['current_museum'] = museum_guess
+    time_range = json_file['museums'][museum_guess]["time_range"]
+    output = render_template(
+        'museum_ticket', museum=museum_guess, time_range=time_range)
+    return question(output)
+
+@ask.intent("NoThanks")
+def museums_tickets_info():
+    global last_node
+    if last_node == "MuseumsOpeningTime":
+        last_node = "NoThanksResendEmail"
+        output = render_template('other_places')
+        return question(output)
+
+
+@ask.intent("YesThanks")
+def yes_thanks():
+    return question("Bye")
+    
+
+@ask.intent("BreweriesInfo")
+def breweries_info():
+    global last_node
+    if last_node == "NoThanksResendEmail" or True:
+        breweries = ", ".join(json_file['breweries'].keys())
+        last_node = "ListBreweries"
+        output = render_template('list_breweries', breweries=breweries)
+        return question(output)
 
 #@ask.intent('ReviewLatestIntent')
 #def launchReview(show):
